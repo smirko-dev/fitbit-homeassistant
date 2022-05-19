9c752af79958eb274d1c342ee1759f8cff24f939
smirko-dev <64351017+smirko-dev@users.noreply.github.com>
2022-05-19 10:40:00 +0200

    Update index.js

--- companion/index.js
+++ companion/index.js
@@ -7,7 +7,7 @@ import { settingsStorage } from "settings";
 import { sendData, isEmpty } from "../common/utils";
 
 var HA = require('./HomeAssistantAPI.js');
-HA = new HomeAssistantAPI("", "8123", "");
+HA = new HomeAssistantAPI("127.0.0.1:8123", "", False);
 
 let URL = "";
 let Port = "8123";
@@ -57,7 +57,7 @@ settingsStorage.onchange = function(evt) {
     else if (evt.key === "entities") {
         sendData({key: "clear"});
         JSON.parse(evt.newValue).forEach(element => {
-            fetchEntity(address(), Token, element["name"]);
+            HA.fetchEntity(element["name"]);
         });
     }
     else if (evt.key === "force") {
@@ -74,126 +74,8 @@ if (companion.launchReasons.settingsChanged) {
     });
     sendData({key: "clear"});
     JSON.parse(settingsStorage?.getItem("entities"))?.forEach((element) => {
-        fetchEntity(address(), Token, element["name"]);
+        HA.fetchEntity(element["name"]);
     });
-}
-
-// Get entity info
-function fetchEntity(url, token, entity) {
-    fetch(`${url}/api/states/${entity}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "content-type": "application/json",
-        }
-    })
-    .then(async(response) => {
-        if (response.ok) {
-            let data = await response.json();
-            let msgData = {
-                key: "add",
-                id: data["entity_id"],
-                name: data["entity_id"],
-                state: data["state"],
-            };
-            if (data["attributes"] && data["attributes"]["friendly_name"]) {
-                msgData.name = data["attributes"]["friendly_name"];
-            }
-            if (data["entity_id"].startsWith("script")) {
-                msgData.state = 'exe'
-            }
-            else if (data["entity_id"].startsWith("automation")) {
-                msgData.state = 'exe'
-            }
-            sendData(msgData);
-        }
-        else {
-            console.log(`[fetchEntity] ${gettext("error")} ${response.status}`);
-        }
-    })
-    .catch(err => console.log('[fetchEntity]: ' + err));
-}
-
-// Get Availability of HA
-function fetchApiStatus(url, token) {
-    fetch(`${url}/api/config`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "content-type": "application/json",
-        }
-    })
-    .then(async(response) => {
-        let data = await response.json();
-        if (response.status === 200) {
-            sendData({key: "api", value: "ok", name: data["location_name"]});
-        }
-        else {
-            const json = JSON.stringify({
-                key: "api",
-                value: `${gettext("error")} ${response.status}`
-            });
-            sendData(json);
-        }
-    })
-    .catch(err => {
-        console.log('[fetchApiStatus]: ' + err);
-        sendData({key: "api", value: gettext("connection_error")});
-    })
-}
-
-// Change entity state
-function changeEntity(url, token, entity, state) {
-    const json = JSON.stringify({
-        entity_id: `${entity}`
-    });
-    const domain = entity.split('.')[0]
-    const group = groups[domain]
-    state = nextStateOverrides[domain] || state
-    //DEBUG console.log(`SENT ${url}/api/services/${group}/${state} FOR ${entity}`);
-    fetch(`${url}/api/services/${group}/${state}`, {
-        method: "POST",
-        body: json,
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "content-type": "application/json",
-        }
-    })
-    .then(async(response) => {
-        if (response.ok) {
-            let data = await response.json();
-            //DEBUG console.log('RECEIVED ' + JSON.stringify(data));
-            if (Force) {
-                let msgData = {
-                    key: "change",
-                    id: entity,
-                    state: forcedStates[state] || state,
-                };
-                if (!entity.startsWith("script") && !entity.startsWith("automation")) {
-                    //DEBUG console.log('FORCED ' + JSON.stringify(msgData));
-                    sendData(msgData);
-                }
-            }
-            else if (!isEmpty(data)) {
-                data.forEach(element => {
-                    if (element["entity_id"] === entity) {
-                        let msgData = {
-                            key: "change",
-                            id: element["entity_id"],
-                            state: element["state"],
-                        };
-                        if (!element["entity_id"].startsWith("script") && !element["entity_id"].startsWith("automation")) {
-                            sendData(msgData);
-                        }
-                    }
-                })
-            }
-        }
-        else {
-            console.log(`[changeEntity] ${gettext("error")} ${response.status}`);
-        }
-    })
-    .catch(err => console.log('[changeEntity]: ' + err));
 }
 
 // Message socket opens
@@ -210,39 +92,38 @@ messaging.peerSocket.onclose = () => {
 messaging.peerSocket.onmessage = evt => {
     console.log('Received', JSON.stringify(evt.data));
     if (evt.data.key === "change") {
-        changeEntity(address(), Token, evt.data.entity, evt.data.state);
+        HA.changeEntity(evt.data.entity, evt.data.state);
     }
     else if (evt.data.key === "url") {
         URL = evt.data.value;
+        HA.setup(address(), Token, Force);
         if (URL && Port && Token) {
-            fetchApiStatus(address(), Token);
-            HA.setup(address(), Token, Force);
+            HA.fetchApiStatus()
         }
     }
     else if (evt.data.key === "port") {
         Port = evt.data.value;
+        HA.setup(address(), Token, Force);
         if (URL && Port && Token) {
-            fetchApiStatus(address(), Token);
-            HA.setup(address(), Token, Force);
+            HA.fetchApiStatus()
         }
     }
     else if (evt.data.key === "token") {
         Token = evt.data.value;
+        HA.setup(address(), Token, Force);
         if (URL && Port && Token) {
-            fetchApiStatus(address(), Token);
-            HA.setup(address(), Token, Force);
+            HA.fetchApiStatus()
         }
     }
     else if (evt.data.key === "entities") {
         if (evt.data.value) {
             sendData({key: "clear"});
             evt.data.value.forEach(element => {
-                fetchEntity(address(), Token, element["name"]);
+                HA.fetchEntity(element["name"]);
             })
         }
     }
     else if (evt.data.key === "force") {
-        Force = evt.data.value;
-        HA.setup(address(), Token, Force);
+        HA.setup(address(), Token, evt.data.value);
     }
 };
