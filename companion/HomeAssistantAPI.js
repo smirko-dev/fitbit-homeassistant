86e05d240374caa88203c9b82d892c242f9a59c1
smirko-dev <64351017+smirko-dev@users.noreply.github.com>
2022-05-20 10:05:03 +0200

    Update HomeAssistantAPI.js

--- companion/HomeAssistantAPI.js
+++ companion/HomeAssistantAPI.js
@@ -88,120 +88,126 @@ HomeAssistantAPI.prototype.address = function() {
 
 HomeAssistantAPI.prototype.fetchEntity = function(entity) {
     let self = this;
-    fetch(`${self.address()}/api/states/${entity}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${self.token}`,
-            "content-type": "application/json",
-        }
-    })
-    .then(async(response) => {
-        if (response.ok) {
-            let data = await response.json();
-            let msgData = {
-                key: "add",
-                id: data["entity_id"],
-                name: data["entity_id"],
-                state: data["state"],
-            };
-            if (data["attributes"] && data["attributes"]["friendly_name"]) {
-                msgData.name = data["attributes"]["friendly_name"];
+    if (self.isValid()) {
+        fetch(`${self.address()}/api/states/${entity}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${self.token}`,
+                "content-type": "application/json",
             }
-            if (data["entity_id"].startsWith("script")) {
-                msgData.state = 'exe'
+        })
+        .then(async(response) => {
+            if (response.ok) {
+                let data = await response.json();
+                let msgData = {
+                    key: "add",
+                    id: data["entity_id"],
+                    name: data["entity_id"],
+                    state: data["state"],
+                };
+                if (data["attributes"] && data["attributes"]["friendly_name"]) {
+                    msgData.name = data["attributes"]["friendly_name"];
+                }
+                if (data["entity_id"].startsWith("script")) {
+                    msgData.state = 'exe'
+                }
+                else if (data["entity_id"].startsWith("automation")) {
+                    msgData.state = 'exe'
+                }
+                sendData(msgData);
             }
-            else if (data["entity_id"].startsWith("automation")) {
-                msgData.state = 'exe'
+            else {
+                console.log(`[fetchEntity] ${gettext("error")} ${response.status}`);
             }
-            sendData(msgData);
-        }
-        else {
-            console.log(`[fetchEntity] ${gettext("error")} ${response.status}`);
-        }
-    })
-    .catch(err => console.log('[fetchEntity]: ' + err));
+        })
+        .catch(err => console.log('[fetchEntity]: ' + err));
+    }
 }
 
 HomeAssistantAPI.prototype.fetchApiStatus = function() {
     let self = this;
-    fetch(`${self.address()}/api/config`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${self.token}`,
-            "content-type": "application/json",
-        }
-    })
-    .then(async(response) => {
-        let data = await response.json();
-        if (response.status === 200) {
-            sendData({key: "api", value: "ok", name: data["location_name"]});
-        }
-        else {
-            const json = JSON.stringify({
-                key: "api",
-                value: `${gettext("error")} ${response.status}`
-            });
-            sendData(json);
-        }
-    })
-    .catch(err => {
-        console.log('[fetchApiStatus]: ' + err);
-        sendData({key: "api", value: gettext("connection_error")});
-    })
+    if (self.isValid()) {
+        fetch(`${self.address()}/api/config`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${self.token}`,
+                "content-type": "application/json",
+            }
+        })
+        .then(async(response) => {
+            let data = await response.json();
+            if (response.status === 200) {
+                sendData({key: "api", value: "ok", name: data["location_name"]});
+            }
+            else {
+                const json = JSON.stringify({
+                    key: "api",
+                    value: `${gettext("error")} ${response.status}`
+                });
+                sendData(json);
+            }
+        })
+        .catch(err => {
+            console.log('[fetchApiStatus]: ' + err);
+            sendData({key: "api", value: gettext("connection_error")});
+        })
+    }
 }
 
 HomeAssistantAPI.prototype.changeEntity = function(entity, state) {
     let self = this;
-    const json = JSON.stringify({
-        entity_id: `${entity}`
-    });
-    const domain = entity.split('.')[0];
-    const group = Groups[domain];
-    state = NextStateOverrides[domain] || state;
-    //DEBUG console.log(`SENT ${url}/api/services/${group}/${state} FOR ${entity}`);
-    fetch(`${self.address()}/api/services/${group}/${state}`, {
-        method: "POST",
-        body: json,
-        headers: {
-            "Authorization": `Bearer ${self.token}`,
-            "content-type": "application/json",
-        }
-    })
-    .then(async(response) => {
-        if (response.ok) {
-            let data = await response.json();
-            //DEBUG console.log('RECEIVED ' + JSON.stringify(data));
-            if (self.force) {
-                let msgData = {
-                    key: "change",
-                    id: entity,
-                    state: ForcedStates[state] || state,
-                };
-                if (!entity.startsWith("script") && !entity.startsWith("automation")) {
-                    //DEBUG console.log('FORCED ' + JSON.stringify(msgData));
-                    sendData(msgData);
+    if (self.isValid()) {
+        const json = JSON.stringify({
+            entity_id: `${entity}`
+        });
+        const domain = entity.split('.')[0];
+        const group = Groups[domain];
+        state = NextStateOverrides[domain] || state;
+        //DEBUG console.log(`SENT ${url}/api/services/${group}/${state} FOR ${entity}`);
+        fetch(`${self.address()}/api/services/${group}/${state}`, {
+            method: "POST",
+            body: json,
+            headers: {
+                "Authorization": `Bearer ${self.token}`,
+                "content-type": "application/json",
+            }
+        })
+        .then(async(response) => {
+            if (response.ok) {
+                let data = await response.json();
+                //DEBUG console.log('RECEIVED ' + JSON.stringify(data));
+                if (self.force) {
+                    let msgData = {
+                        key: "change",
+                        id: entity,
+                        state: ForcedStates[state] || state,
+                    };
+                    if (!entity.startsWith("script") && !entity.startsWith("automation")) {
+                        //DEBUG console.log('FORCED ' + JSON.stringify(msgData));
+                        sendData(msgData);
+                    }
+                }
+                else if (!isEmpty(data)) {
+                    data.forEach(element => {
+                        if (element["entity_id"] === entity) {
+                            let msgData = {
+                                key: "change",
+                                id: element["entity_id"],
+                                state: element["state"],
+                            };
+                            if (!element["entity_id"].startsWith("script") && !element["entity_id"].startsWith("automation")) {
+                                sendData(msgData);
+                            }
+                        }
+                    })
                 }
             }
-            else if (!isEmpty(data)) {
-                data.forEach(element => {
-                    if (element["entity_id"] === entity) {
-                        let msgData = {
-                            key: "change",
-                            id: element["entity_id"],
-                            state: element["state"],
-                        };
-                        if (!element["entity_id"].startsWith("script") && !element["entity_id"].startsWith("automation")) {
-                            sendData(msgData);
-                        }
-                    }
-                })
+            else {
+                console.log(`[changeEntity] ${gettext("error")} ${response.status}`);
             }
-        }
-        else {
-            console.log(`[changeEntity] ${gettext("error")} ${response.status}`);
-        }
-    })
-    .catch(err => console.log('[changeEntity]: ' + err));
+        })
+        .catch(err => console.log('[changeEntity]: ' + err));
+    }
 }
 
 module.exports = HomeAssistantAPI;
